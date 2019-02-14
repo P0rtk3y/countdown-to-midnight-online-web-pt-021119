19c9540a56ecd8341ff19c5058f3e459105eb1e5
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-14 17:10:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,16 @@ def countdown(number)
   while number > -1 
     puts "#{number} SECOND(S)"
     number -= 1 
-    sleep 1
   end 
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+ number = 10
+  while number > -1 
+    puts "#{number} SECOND(S)"
+    number -= 1 
+    sleep 1
+  end 
+  "HAPPY NEW YEAR!"
+end  
